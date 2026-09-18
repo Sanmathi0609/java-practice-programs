fa8cf7993ba415ac3bd0262770e092ff4ea37cbd
Sanmathi R <sanmathiramasamy06@gmail.com>
2026-09-18 19:38:21 +0530

    Reverse string

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] s) {
+      
+      int start=0;
+      int end=s.length-1;
+      while(start<end){
+        char t=s[start];
+        s[start]=s[end];
+        s[end]=t;
+
+        start++;
+        end--;
+      }
+  }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
