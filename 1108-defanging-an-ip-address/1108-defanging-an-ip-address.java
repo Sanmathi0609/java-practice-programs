6f37b393a47c9f0d280bca30ed153022502f8bf8
Sanmathi R <sanmathiramasamy06@gmail.com>
2026-09-21 19:19:34 +0530

    Defanging an IP address

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        String ip= address.replace(".","[.]");
+        return ip;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
